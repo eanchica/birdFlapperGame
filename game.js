cde712b9c50f2dfd4a29e908cb4d4e0a70fd7d88
eanchica <60475527+eanchica@users.noreply.github.com>
2021-03-04 12:27:04 -0500

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ let tube = document.getElementById("tube");
 let open = document.getElementById("open");
 
 open.addEventListener('animationIteration' , () => {
-    let random = -((Math.random() * 300) + 150); 
+    let random = -((Math.random() * 300) + 200); 
     open.style.top = random + "px";
 });
 
